8dcb338d9b32f973ce16968d7cbc23374c1ceab5
Ashhar Ansari <ansifrah@gmail.com>
2026-03-31 03:57:08 +0530

    modifies tokenizer to handle single quotes inside single quotes

--- src/interpreter/tokenizer.cpp
+++ src/interpreter/tokenizer.cpp
@@ -1,18 +1,41 @@
 #include "interpreter/interpreter.h"
 
-vector<string> tokenize(const string& s)
+vector<string> tokenize(const string &s)
 {
     vector<string> tokens;
     string x = "";
     bool in_quotes = false;
     char quote_char = 0;
+    size_t size = s.size();
+    bool check = false;
 
-    for (size_t i = 0; i < s.size(); i++)
+    for (size_t i = 0; i < size; i++)
     {
+        check = false;
         char c = s[i];
 
         if ((c == '\'') && !in_quotes)
         {
+            for (size_t j = i + 1; j < size; j++)
+            {
+                for (size_t k = j + 2; k < size; k++)
+                {
+                    if ((j + 1 < size) && s[j] == '\\' && s[j + 1] == '\'' && s[k] == '\'')
+                    {
+                        check = true;
+                        break;
+                    }
+                }
+                if (s[j] == '\'')
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if (!check)
+            {
+                goto only_1_quote;
+            }
             if (!x.empty())
             {
                 tokens.push_back(x);
@@ -24,21 +47,32 @@ vector<string> tokenize(const string& s)
             continue;
         }
 
-        if (in_quotes && c == quote_char)
-        {
-            tokens.push_back(x);
-            x = "";
-            tokens.push_back("'");
-            in_quotes = false;
-            continue;
-        }
-
         if (in_quotes)
         {
+            if (c == '\\' && (i + 1) < size)
+            {
+                char next = s[i + 1];
+                if (next == quote_char || next == '\\')
+                {
+                    x += next;
+                    i++;
+                    continue;
+                }
+            }
+
+            if (c == quote_char)
+            {
+                tokens.push_back(x);
+                x = "";
+                tokens.push_back(string(1, quote_char));
+                in_quotes = false;
+                continue;
+            }
+
             x += c;
             continue;
         }
-
+    only_1_quote:
         if (isspace(c))
         {
             if (!x.empty())
@@ -47,7 +81,7 @@ vector<string> tokenize(const string& s)
                 x = "";
             }
         }
-        else if (c == ',' || c == '(' || c == ')' || c == ';' || c == '='|| c=='-')
+        else if (c == ',' || c == '(' || c == ')' || c == ';' || c == '=' || c == '-')
         {
             if (!x.empty())
             {
